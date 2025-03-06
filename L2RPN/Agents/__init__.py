cd93eaa12bb9c31f0501f7818c021b2462ed0b24
Arvid Rolander <arvid.rolander@gmail.com>
2025-03-06 10:51:06 +0100

    Add: Agent implementations and replay buffer

--- L2RPN/Agents/__init__.py
+++ L2RPN/Agents/__init__.py
@@ -1,12 +1,7 @@
 __all__ = [
-    "DoNothingAgent", "ModularDQN", "RainbowDQN",
-    "DummyAgent", "DummyActionSpace", "DummyEnv", "DummyObservation",
-    "ExhaustiveAgent",
-    "ProximalAgent",
+    "DDPG", "SAC", "TD3", "DDPGParams", "TD3HParams", "SACHParams"
 ]
 
-from L2RPN.Agents.DoNothing import DoNothingAgent
-from L2RPN.Agents.DQN import ModularDQN, RainbowDQN
-from L2RPN.Agents.Dummies import DummyAgent, DummyActionSpace, DummyEnv, DummyObservation
-from L2RPN.Agents.Exhaustive import ExhaustiveAgent
-from L2RPN.Agents.PPO import ProximalAgent
+from L2RPN.Agents.DDPG import DDPG, DDPGParams
+from L2RPN.Agents.TD3 import TD3, TD3HParams
+from L2RPN.Agents.SAC import SAC, SACHParams
---
+++ L2RPN/ReplayBuffers/__init__.py
@@ -0,0 +1,5 @@
+__all__ = [
+    "ReplayBuffer"
+]
+
+from VanillaReplayBuffer import ReplayBuffer
